a40d3726f5f92366b7c332a5d53534243e383297
tojinnykim <tojinnykim@github.com>
2025-09-21 10:05:50 +0900

    짝수 값 출력 로직 추가 및 학생 정보 출력 로직 정리

--- 2.파이썬 기본문법/0920_review.py
+++ 2.파이썬 기본문법/0920_review.py
@@ -1,42 +1,42 @@
-# # nums = [1, 2, 3, 4, 5, 6]
-# # print(f"numbers = {nums}")
+nums = [1, 2, 3, 4, 5, 6]
+print(f"numbers = {nums}")
 
 
 
-# # even_list = []
-# # for num in nums:
-# #     if num % 2 == 0:
-# #         even_list.append(num)
-# # print(f"짝수의 값 = {even_list}")
+even_list = []
+for num in nums:
+    if num % 2 == 0:
+        even_list.append(num)
+print(f"짝수의 값 = {even_list}")
 
 
 
-# # name = "Alice"
-# # scores = [80, 90, 100]
-# # average = sum(scores) / len(scores) 
+name = "Alice"
+scores = [80, 90, 100]
+average = sum(scores) / len(scores) 
 
-# # print(f"Student: {name}, Scores: {scores}, Average: {average}")
-
-
-
-# students = [
-#     {"name":"jinny", "scores":[80,90,95]},
-#     {"name":"tom", "scores":[20,45,80]}
-#     ]
-# for student in students:
-#     average = sum(student["scores"]) / len(student["scores"])
-#     print(f"Student : {student['name']}, Scores: {student['scores']}, Average: {average}")
+print(f"Student: {name}, Scores: {scores}, Average: {average}")
 
 
 
-# students = [
-#     {"name":"jinny", "scores":[80,90,95]},
-#     {"name":"tom", "scores":[20,45,80]},
-#     {"name":"jason", "scores":[35,65,90]}
-#     ]
-# for student in students:
-#     average = sum(student["scores"]) / len(student["scores"])
-#     print(f"Student : {student['name']}, Average: {average:.1f}")
+students = [
+    {"name":"jinny", "scores":[80,90,95]},
+    {"name":"tom", "scores":[20,45,80]}
+    ]
+for student in students:
+    average = sum(student["scores"]) / len(student["scores"])
+    print(f"Student : {student['name']}, Scores: {student['scores']}, Average: {average}")
+
+
+
+students = [
+    {"name":"jinny", "scores":[80,90,95]},
+    {"name":"tom", "scores":[20,45,80]},
+    {"name":"jason", "scores":[35,65,90]}
+    ]
+for student in students:
+    average = sum(student["scores"]) / len(student["scores"])
+    print(f"Student : {student['name']}, Average: {average:.1f}")
 
 students = [
     {"name":"jinny", "scores":[80,90,95]},
